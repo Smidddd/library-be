c3b094bf2d5d44ce786149da845448af6ab06f61
smidd <smido209@gmail.com>
2023-02-13 17:39:10 +0100

    Uloha prog zaklad

--- library/src/main/java/sk/umb/example/library/LibraryApplication.java
+++ library/src/main/java/sk/umb/example/library/LibraryApplication.java
@@ -1,4 +1,4 @@
-package com.example.umb.library;
+package sk.umb.example.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello world");
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
--- library/src/test/java/sk/umb/example/library/LibraryApplicationTests.java
+++ library/src/test/java/sk/umb/example/library/LibraryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.umb.library;
+package sk.umb.example.library;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
