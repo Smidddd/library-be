c97e2a3fb2aa113384a664c09534170356dddd3b
majo <mariochrusc@gmail.com>
2023-02-27 00:02:10 +0100

    Category

--- library/src/main/java/sk/umb/example/library/category/controller/CategoryController.java
+++ library/src/main/java/sk/umb/example/library/category/controller/CategoryController.java
@@ -1,34 +1,46 @@
 package sk.umb.example.library.category.controller;
 
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import sk.umb.example.library.category.service.CategoryDetailDTO;
+import sk.umb.example.library.category.service.CategoryRequestDTO;
+import sk.umb.example.library.category.service.CategoryService;
 
 import java.util.Collections;
 import java.util.List;
 
 @RestController
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
     @GetMapping("/api/category/list")
-    public List listCategories(@RequestParam(required = false) String lastName){
+    public List <CategoryDetailDTO> listCategories(@RequestParam(required = false) String name){
         System.out.println("List categories called");
 
-        return Collections.emptyList();
+        return Strings.isEmpty(name)? categoryService.getAllCategories()
+                                    : categoryService.searchCategoryByName(name);
     }
-    @GetMapping("/api/category/retrieve")
-    public List retrieveCategory(@RequestParam(required = false) String lastName){
-        System.out.println("Details of category called");
+    @GetMapping("/api/category/{categoryId}")
+    public CategoryDetailDTO retrieveCategory(@PathVariable Long categoryId){
+        System.out.println("Details of category called + ");
 
-        return Collections.emptyList();
+        return categoryService.retrieveCategory(categoryId);
     }
     @PostMapping("/api/category")
-    public void createCategory(){
+    public Long createCategory(@RequestBody CategoryRequestDTO categoryRequestDTO){
         System.out.println("Create category called");
+        return categoryService.createCategory(categoryRequestDTO);
     }
     @PutMapping("/api/category/{categoryId}")
-    public void updateCategory(@PathVariable Long categoryId){
+    public void updateCategory(@PathVariable Long categoryId, @RequestBody CategoryRequestDTO categoryRequestDTO){
         System.out.println("Update category called ID: "+ categoryId);
+        categoryService.updateCategory(categoryId, categoryRequestDTO);
     }
     @DeleteMapping("/api/category/{categoryId}")
     public void deleteCategory(@PathVariable Long categoryId){
         System.out.println("Delete category called ID: "+ categoryId);
+        categoryService.deleteCategory(categoryId);
     }
 }
---
+++ library/src/main/java/sk/umb/example/library/category/service/CategoryDetailDTO.java
@@ -0,0 +1,26 @@
+package sk.umb.example.library.category.service;
+
+import java.lang.reflect.Array;
+
+public class CategoryDetailDTO {
+    private Long id;
+    private String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
